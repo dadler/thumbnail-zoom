dadler/thumbnail-zoom

commit ae1e76010219635dc42800003db72684f460d7fc

1. Added netflix; 2. prevent popup in Google search field; 3. support in Thumbnail sites with 000/014/111/004_160.jpg

src/resources/pages.js
@@ -324,6 +324,49 @@ ThumbnailZoomPlus.Pages.MySpace = {
 };
 
 /**
+ * Netflix
+ */
+ThumbnailZoomPlus.Pages.Netflix = {
+  key: "netflix",
+  name: "Netflix",
+  host: /^(.*\.)?netflix\.com$/,
+  imageRegExp: new RegExp("://movies\\.netflix\\.com/WiPlayer\\?movieid=|" +
+                          "://movies\\.netflix\\.com/WiMovie/.*/([0-9]+)\\?.*|" +
+                          "\\.nflximg\\.com/.*" + ThumbnailZoomPlus.Pages._imageTypesRegExpStr + "$"),
+                          
+  getImageNode : function(aNode, nodeName, nodeClass) {
+    if (nodeName == "a" || nodeName == "img") {
+      return aNode;
+    }
+    return null;
+  },
+
+  getZoomImage : function(aImageSrc) {
+
+    // For static thumbs
+    // http://cdn-2.nflximg.com/en_us/boxshots/large/60024022.jpg becomes
+    // http://cdn-2.nflximg.com/en_us/boxshots/ghd/60024022.jpg
+    let netflixRex1 = new RegExp("(\.nflximg.com/.*/boxshots)/(large|[0-9]+)/");
+    aImageSrc = aImageSrc.replace(netflixRex1, "$1/ghd/");
+
+    let netflixRex2 = new RegExp("(\.nflximg.com/.*/kidscharacters)/(small|main|[0-9]+)/");
+    aImageSrc = aImageSrc.replace(netflixRex2, "$1/cdp/");
+    
+    // For movie thumbs with "play" icons / links
+    // Change http://movies.netflix.com/WiPlayer?movieid=70128681&amp;trkid=3651203&amp;nscl=1 to
+    // http://cdn-1.nflximg.com/en_us/boxshots/ghd/70128681.jpg
+    // and http://movies.netflix.com/WiMovie/Phineas_Ferb/70177007?trkid=4009636
+    // to http://cdn-1.nflximg.com/en_us/boxshots/ghd/70177007.jpg
+    let netflixRex3 = new RegExp(".*//movies\\.netflix\\.com/WiPlayer\\?movieid=([0-9]+).*");
+    let netflixRex4 = new RegExp(".*//movies\\.netflix\\.com/WiMovie/.*/([0-9]+)\\?.*");
+    aImageSrc = aImageSrc.replace(netflixRex3, "http://cdn-1.nflximg.com/en_us/boxshots/ghd/$1.jpg");
+    aImageSrc = aImageSrc.replace(netflixRex4, "http://cdn-1.nflximg.com/en_us/boxshots/ghd/$1.jpg");
+
+    return aImageSrc;
+  }
+};
+
+/**
  * Flickr
  */
 ThumbnailZoomPlus.Pages.Flickr = {
@@ -814,7 +857,7 @@ ThumbnailZoomPlus.Pages.Others = {
                       "[\?&]img_?url=|" +
                       "(https?)://(?!(?:www|today|groups|muro|chat|forum|critiques|portfolio|help|browse)\\.)" +
                           "([^/?&.])([^/?&.])([^/?&.]*)\\.deviantart\\.com/?$|" +
-                      "stumbleupon.com\/(to|su)\/[^\/]+\/(.*" + ThumbnailZoomPlus.Pages._imageTypesRegExpStr + ")",
+                      "stumbleupon.com\/(to|su)\/[^\/]+\/(.*" + ThumbnailZoomPlus.Pages._imageTypesRegExpStr + ")" +
                       "i"),
 
   _logger: ThumbnailZoomPlus.Pages._logger,
@@ -996,7 +1039,7 @@ ThumbnailZoomPlus.Pages.Others = {
       }
       aImageSrc = aImageSrc.replace(youtubeEx, "$1i3.ytimg.com/vi/$2/hqdefault.jpg");
     }
-        
+  
     // If imgur link, remove part after "&" or "#", e.g. for https://imgur.com/nugJJ&yQU0G
     // Also turn http://imgur.com/gallery/24Av1.jpg into http://imgur.com/24Av1.jpg
     let imgurRex = new RegExp(/(imgur\.com\/)(gallery\/)?([^\/&#]+)([&#].*)?/);
@@ -1051,6 +1094,10 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
       // it to be an actual image.  (The Others rule already handles hrefs.)
       return null;
     }
+    if ("gbqfif" == nodeClass // google search field.
+        ) {
+      return null;
+    }
     return aNode;
   },
   
@@ -1060,6 +1107,11 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
     // work since it'd instead follow the <a> link around the image.
     let regEx = new RegExp("(/images)/(thumb|mini)/([0-9]+/[0-9]+/[0-9]+\.)");
     aImageSrc = aImageSrc.replace(regEx, "$1/full/$3");
+    
+    // For some sites, change 000/014/111/004_160.jpg to 000/014/111/004_1000.jpg
+    let regEx = new RegExp("(/[0-9]+/[0-9]+/[0-9]+/[0-9]+)_[0-9]{1,3}(\.[a-z]+)");
+    aImageSrc = aImageSrc.replace(regEx, "$1_1000$2");
+    
     return aImageSrc; 
   }
 

src/chrome/content/options.xul
@@ -79,6 +79,8 @@
         name="extensions.thumbnailzoomplus.picasa.enable" type="bool"/>
       <preference id="thumbnailzoomplus-options-myspace-pref"
         name="extensions.thumbnailzoomplus.myspace.enable" type="bool"/>
+      <preference id="thumbnailzoomplus-options-netflix-pref"
+        name="extensions.thumbnailzoomplus.netflix.enable" type="bool"/>
       <preference id="thumbnailzoomplus-options-flickr-pref"
         name="extensions.thumbnailzoomplus.flickr.enable" type="bool"/>
       <preference id="thumbnailzoomplus-options-wikipedia-pref"
@@ -250,14 +252,17 @@
           <checkbox label="MySpace"
             preference="thumbnailzoomplus-options-myspace-pref" />
 
+          <checkbox label="Netflix"
+            preference="thumbnailzoomplus-options-netflix-pref" />
+
           <checkbox label="okCupid"
             preference="thumbnailzoomplus-options-okcupid-pref" />
 
-          <checkbox label="PhotoBucket"
-            preference="thumbnailzoomplus-options-photobucket-pref" />
-
         </vbox>
         <vbox class="thumbnailzoomplus-options-pages-column" flex="1">
+
+          <checkbox label="PhotoBucket"
+            preference="thumbnailzoomplus-options-photobucket-pref" />
 
           <checkbox label="Pinterest"
             preference="thumbnailzoomplus-options-pinterest-pref" />
@@ -274,11 +279,11 @@
           <checkbox label="Twitpic"
             preference="thumbnailzoomplus-options-twitpic-pref" />
 
-          <checkbox label="Twitter"
-            preference="thumbnailzoomplus-options-twitter-pref" />
-
         </vbox>
         <vbox class="thumbnailzoomplus-options-pages-column" flex="1">
+
+          <checkbox label="Twitter"
+            preference="thumbnailzoomplus-options-twitter-pref" />
 
           <checkbox label="YouTube"
             preference="thumbnailzoomplus-options-youtube-pref" />

src/defaults/preferences/thumbnailzoomplus.js
@@ -59,6 +59,7 @@ pref("extensions.thumbnailzoomplus.twitter.enable",     true);
 pref("extensions.thumbnailzoomplus.youtube.enable",     true);
 pref("extensions.thumbnailzoomplus.twitpic.enable",     true);
 pref("extensions.thumbnailzoomplus.myspace.enable",     true);
+pref("extensions.thumbnailzoomplus.netflix.enable",     true);
 pref("extensions.thumbnailzoomplus.okcupid.enable",     true);
 pref("extensions.thumbnailzoomplus.facebook.enable",    true);
 pref("extensions.thumbnailzoomplus.linkedin.enable",    true);

src/resources/filterService.js
@@ -59,15 +59,16 @@ ThumbnailZoomPlus.FilterService = {
     ThumbnailZoomPlus.Pages.LastFM,
     ThumbnailZoomPlus.Pages.LinkedIn,
     ThumbnailZoomPlus.Pages.MySpace,
+    ThumbnailZoomPlus.Pages.Netflix,
     ThumbnailZoomPlus.Pages.OkCupid,
     ThumbnailZoomPlus.Pages.PhotoBucket,
     ThumbnailZoomPlus.Pages.Pinterest,
-    ThumbnailZoomPlus.Pages.Photosight, // 19
+    ThumbnailZoomPlus.Pages.Photosight, // 20
     ThumbnailZoomPlus.Pages.Picasa,
     ThumbnailZoomPlus.Pages.Tagged,
     ThumbnailZoomPlus.Pages.Twitpic,
     ThumbnailZoomPlus.Pages.Twitter,
-    ThumbnailZoomPlus.Pages.YouTube, // 24
+    ThumbnailZoomPlus.Pages.YouTube, // 25
     ThumbnailZoomPlus.Pages.Wikipedia,
     
     // The next two must be last so they are lower priority.

src/test/TestCases.html
@@ -204,6 +204,13 @@ jpg profile pic (but not if it's gif unfortunately).
 
 <h2>MySpace</h2>
 
+<h2>Netflix</h2>
+Test on:
+<ul>
+<li><a href="http://movies.netflix.com/WiHome?lnkctr=mhWN">Watch Instantly's boxe icons with play buttons</a> 
+<li><a href="http://movies.netflix.com/Kids">Shaun The Sheep and similar graphics and movie boxes in Just For Kids</a>
+<li><a href="http://www2.netflix.com/BrowseDVDSelection">Box covers without Play buttons in Browse DVDs</a>
+</ul>
 
 <h2>LinkedIn</h2>
 <li><a href="http://www.linkedin.com/pub/dir/?first=john&last=doe&search=Search&searchType=fps">

commit a1c5ae148f0d4beb6c0a141b04878e79b2a2139c

Improved google play.  Fixed bug introduced in ae1e7601 Feb 22; bug made 'others' img test work with lowercase only and broke stumbleupon.

src/resources/pages.js
@@ -89,7 +89,8 @@ ThumbnailZoomPlus.Pages._imageTypesRegExpStr = "(?:\\.gif|\\.jpe?g|\\.png|\\.bmp
       as the source automatically.  Returning null prevents popup.
        
     * getImageNode: optional function(aNode, nodeName, nodeClass).  Returns the
-      node from which the popup image's link will be generated.  Useful when
+      node from which the popup image's link will be generated, or null if
+      none.  Useful when
       it's generated not from the direct thumbnail image, but an ancestor or
       peer node.  The image URL will be extracted (in fiterService.js) from the 
       returned node's src, href, or background image.  The default function returns
@@ -916,7 +917,7 @@ ThumbnailZoomPlus.Pages.Others = {
   imageRegExp: new RegExp(ThumbnailZoomPlus.Pages._imageTypesRegExpStr + "([?&].*)?$|" +
                       "tumblr.com/(photo/|tumblr_)|" +
                       "imgur\\.com/(gallery/)?(?!gallery|tools|signin|register|tos$|contact|removalrequest|faq$)" +
-                          "[^/&\\?]+(&.*)?$|" +
+                      "[^/&\\?]+(&.*)?$|" +
                       "(?:www\\.(nsfw)?youtube\\.com|youtu.be)/watch.*(?:v=|/)([^&#!/]+)[^/]*/*$|" +
                       "/youtu.be/[^/]+$|" +
                       "quickmeme\\.com/meme/|" +
@@ -926,9 +927,9 @@ ThumbnailZoomPlus.Pages.Others = {
                       "^https?://twitter.com/.*\\?url=([^&]+)(&.*)?$|" +
                       "[\?&]img_?url=|" +
                       "(https?)://(?!(?:www|today|groups|muro|chat|forum|critiques|portfolio|help|browse)\\.)" +
-                          "([^/?&.])([^/?&.])([^/?&.]*)\\.deviantart\\.com/?$|" +
-                      "stumbleupon.com\/(to|su)\/[^\/]+\/(.*" + ThumbnailZoomPlus.Pages._imageTypesRegExpStr + ")" +
-                      "i"),
+                      "([^/?&.])([^/?&.])([^/?&.]*)\\.deviantart\\.com/?$|" +
+                      "stumbleupon.com\/(to|su)\/[^\/]+\/(.*" + ThumbnailZoomPlus.Pages._imageTypesRegExpStr + ")"
+                      , "i"),
 
   _logger: ThumbnailZoomPlus.Pages._logger,
   
@@ -1283,6 +1284,15 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
     let regEx = new RegExp("xh[a-z0-9]*ster.com.*(/[0-9]+/[0-9]+/[0-9]+/[0-9]+)_[0-9]{1,3}(\.[a-z]+)");
     aImageSrc = aImageSrc.replace(regEx, "$1_1000$2");
     
+    // Google Play album: change
+    // https://lh4.googleusercontent.com/Z0AD4MsVIa8qoMs69GmZqNRHq-dzapfbO_HrviLyBmmbgnwi1_YmhId29CojSoERSbdrqEMonBU=w128 to
+    // https://lh4.googleusercontent.com/Z0AD4MsVIa8qoMs69GmZqNRHq-dzapfbO_HrviLyBmmbgnwi1_YmhId29CojSoERSbdrqEMonBU=w1000
+    // and
+    // https://encrypted.google.com/books?id=bgMiAFs66bwC&printsec=frontcover&img=2&zoom=2&source=ge-web-market to
+    // https://encrypted.google.com/books?id=bgMiAFs66bwC&printsec=frontcover&img=2&zoom=0&source=ge-web-market
+    aImageSrc = aImageSrc.replace(/(\.googleusercontent\.com\/.*=)w[0-9][0-9][0-9]?$/, "$1w1000");
+    aImageSrc = aImageSrc.replace(/(\.google\.com\/books?.*)&zoom=1&/, "$1&zoom=0&");
+
     // Using the thumb itself as source; don't annoy the user with
     // "too small" warnings, which would be quite common.
     flags.noTooSmallWarning = true;