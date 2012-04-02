dadler/thumbnail-zoom

commit 14e0fdccff48b18308fe168d9693e86c49698077

Added check to prevent exception when popup source node is the document itself, as I've seen happen sometimes on Google Images when reoading the page.

src/resources/filterService.js
@@ -361,12 +361,12 @@ ThumbnailZoomPlus.FilterService = {
                          "aNode=" + aNode + ", nodeName=" + nodeName +
                          ", nodeClass=" + nodeClass + ", imageSource=" + imageSource);
       imageNode = pageInfo.getImageNode(aNode, nodeName, nodeClass, imageSource);      
-      if (imageNode != aNode) {
+      if (null != imageNode && imageNode != aNode && imageNode.localName) {
         // changed nodes.   If imageNode == null, we're shouldn't do a popup.
+        // and we ignore if localName is null, as sometimes happens if the
+        // returned node is the document itself (seen when reloading Google Images)
         imageSource = null; // we need to re-get imageSource.
         if (imageNode != null) {
-          this._logger.debug("getImageSource: imageNode=" + imageNode);
-
           var nodeName = imageNode.localName;
           let nodeClass = imageNode.getAttribute("class");
           this._logger.debug("getImageSource: after getImageNode, name=" + nodeName + "; src=" +

commit 6f1f0984fe35f538244d271571e3e9220048201e

Fixed bug which caused google maps and potentially other sites to display popups which they shouldn't.  At least part of the problem was just introduced in 14e0fdccff48b18308fe168d9693e86c49698077

src/resources/filterService.js
@@ -361,12 +361,12 @@ ThumbnailZoomPlus.FilterService = {
                          "aNode=" + aNode + ", nodeName=" + nodeName +
                          ", nodeClass=" + nodeClass + ", imageSource=" + imageSource);
       imageNode = pageInfo.getImageNode(aNode, nodeName, nodeClass, imageSource);      
-      if (null != imageNode && imageNode != aNode && imageNode.localName) {
+      if (imageNode != aNode) {
         // changed nodes.   If imageNode == null, we're shouldn't do a popup.
         // and we ignore if localName is null, as sometimes happens if the
         // returned node is the document itself (seen when reloading Google Images)
         imageSource = null; // we need to re-get imageSource.
-        if (imageNode != null) {
+        if (imageNode != null && imageNode.localName) {
           var nodeName = imageNode.localName;
           let nodeClass = imageNode.getAttribute("class");
           this._logger.debug("getImageSource: after getImageNode, name=" + nodeName + "; src=" +
@@ -374,6 +374,7 @@ ThumbnailZoomPlus.FilterService = {
                            "; backgroundImage=" + imageNode.style.backgroundImage +
                            "; class=" + nodeClass);
         } else {
+          imageNode = null;
           this._logger.debug("getImageSource: after getImageNode, imageNode=null; name=" + nodeName + 
                              "; class=" + nodeClass);
         }

src/resources/pages.js
@@ -1151,10 +1151,10 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
   // show.  
   imageRegExp: new RegExp("^(?![^/]*(" +
                           "(//.*\\.google\\.com?[.a-z]*/(.*/)?images/)|" + // google logos
-                          "(//[a-z0-9]+\\.google.com?[.a-z]*/.*[/?]lyrs=.*)|" + // google maps tiles
+                          "(//[a-z0-9]+\\.google\\.com?[.a-z]*/.*[/?&]lyrs=.*)|" + // google maps tiles
                           "(//maps\\.google\\.com?[.a-z]*/.*)|" + // google maps user photo popups, etc.
-                          "(//maps.gstatic.com?[.a-z]*/.*)|" + // google maps button images
-                          "(//sh.deviantart.net/shadow/)|" + // deviantart frame around thumbs
+                          "(//maps\\.gstatic\\.com?[.a-z]*/.*)|" + // google maps button images
+                          "(//sh\\.deviantart\\.net/shadow/)|" + // deviantart frame around thumbs
                           "((.*\.)(ssl-)?images\-amazon\\.com/images/.*/(buttons|gui)/)|" + // amazon buttons
                           "(^data:image/gif;base64,R0lGODlhEAA)" + // LastPass icon in input fields
                           ")).*", "i"),