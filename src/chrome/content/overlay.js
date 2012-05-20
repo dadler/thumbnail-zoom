dadler/thumbnail-zoom

commit a31502424841f33f0fdf0e216ffc2c024711777c

Workaround for i#72: taking focus dismisses popover menu.
Fixes the problem for the two known cases of it, on deviantart.net.
But not a general solution.

Also fixed Thumbnails rule to not show Google search logo.

src/chrome/content/overlay.js
@@ -2192,14 +2192,51 @@ ThumbnailZoomPlusChrome.Overlay = {
    * we get a key event at all.
    */
   _focusThePopup : function(aImageNode) {
-    this._logger.trace("_focusThePopup");
-
     let doc = this._currentWindow.document;
     let focused = doc.activeElement;
 
-    if (focused && focused.tagName != "BODY") {
-      this._logger.debug("_focusThePopup: focused=" + focused + "; sending mouseover evnet");
+    this._logger.debug("_focusThePopup: aImageNode=" + aImageNode +
+                       "; class=" + aImageNode.className + 
+                       "; p.class=" + aImageNode.parentNode.className + 
+                       "; p.p.class=" + aImageNode.parentNode.parentNode.className + 
+                       "; focused=" + focused);
+    let parent = aImageNode.parentNode || aImageNode;
+    let parent = parent.parentNode || parent;
+    if (parent.className.indexOf("deviants") >= 0 ||
+        parent.className.indexOf("grp hh") >= 0) {
+      // This is the deviantart.com Deviants or Groups pulldown menu.
+      // Focusing would immediately dismiss the menu so we don't.
+      // TODO: it'd be better to detect this more generically if we knew how.
+      this._logger.debug("_focusThePopup: not focusing since ancestor class=" + 
+                         parent.className);
+      return;
+    }
+    
+    if (aImageNode && aImageNode.tagName != "BODY") {
+      this._logger.debug("_focusThePopup: aImageNode=" + aImageNode + "; sending mouseover event");
 
+      if (false) {
+        let node = aImageNode;
+        while (node && node.tagName != "HTML") {
+          if (node.onblur) {
+            this._logger.debug("_focusThePopup: not focusing since " + node + 
+                               " has onblur: " + aImageNode.onblur);
+            return;
+          }
+          if (node.onmouseover) {
+            this._logger.debug("_focusThePopup: not focusing since " + node + 
+                               " has onmouseover: " + aImageNode.onmouseover);
+            return;
+          }
+          if (node.onmouseout) {
+            this._logger.debug("_focusThePopup: not focusing since " + node + 
+                               " has onmouseout: " + aImageNode.onmouseout);
+            return;
+          }
+          node = node.parentNode;
+        }
+      }
+      
       // The focused element will lose focus when we give the popup focus.  Make
       // it lose focus now so it'll send the inevitible blur (focus-loss) event.
       // Then send a synthetic mouseover event so Firefox will continue to show

src/resources/pages.js
@@ -1222,7 +1222,7 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
   // Patterns in () must match starting from first slash (or earlier)
   // up to end of entire URL, so typically start with // and end with .* .
   imageRegExp: new RegExp("^(?![^/]*("
-                          + "(//.*\\.google\\.com?[.a-z]*/(.*/)?images/)" // google logos
+                          + "(//.*\\.google\\.com?[.a-z]*/(.*/)?(images|logos)/)" // google logos
                           + "|(//[a-z0-9]+\\.google\\.com?[.a-z]*/.*[/?&]lyrs=.*)" // google maps tiles
                           + "|(//maps\\.google\\.com?[.a-z]*/.*)" // google maps user photo popups, etc.
                           + "|(//maps\\.gstatic\\.com?[.a-z]*/.*)" // google maps button images

commit 88514adddd1e8aeb9a9f2a7c6a6cff0864b7ba6f

Fixed problem in 1.7.2 introduced in a31502424841f3, causing Google's Visual Similar Images to cycle popups

src/chrome/content/overlay.js
@@ -2238,30 +2238,12 @@ ThumbnailZoomPlusChrome.Overlay = {
       return;
     }
     
-    if (aImageNode && aImageNode.tagName != "BODY") {
+    if (focused && focused.tagName != "BODY") {
+      // The previously-focused element wasn't the document itself,
+      // so send synthetic focus events (more details below).  
+      // Note that we test 'focused' rather than 'aImageNode'; testing the 
+      // latter would cause popup cycling on Google's "Visually related images".
       this._logger.debug("_focusThePopup: aImageNode=" + aImageNode + "; sending mouseover event");
-
-      if (false) {
-        let node = aImageNode;
-        while (node && node.tagName != "HTML") {
-          if (node.onblur) {
-            this._logger.debug("_focusThePopup: not focusing since " + node + 
-                               " has onblur: " + aImageNode.onblur);
-            return;
-          }
-          if (node.onmouseover) {
-            this._logger.debug("_focusThePopup: not focusing since " + node + 
-                               " has onmouseover: " + aImageNode.onmouseover);
-            return;
-          }
-          if (node.onmouseout) {
-            this._logger.debug("_focusThePopup: not focusing since " + node + 
-                               " has onmouseout: " + aImageNode.onmouseout);
-            return;
-          }
-          node = node.parentNode;
-        }
-      }
       
       // The focused element will lose focus when we give the popup focus.  Make
       // it lose focus now so it'll send the inevitible blur (focus-loss) event.