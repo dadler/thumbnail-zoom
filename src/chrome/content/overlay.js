58a75f76083da7f59ce61f9ed75414fbbe5e0919
David Adler <browser.add.ons@davidadler.name>
2012-05-25 08:37:35 -0700

    Fixed #75: Popup does not close occasionally
    Fixed two ways:
    1. the real fix: clears out reference to document
    (this._panelCaption.ThumbnailZoomPlusOriginalTitleNode) in
    _onPageLoaded.
    
    2. Robustness: added another exception hander to _closePanel() so
    that even if we get an exception in close, it'll still popdown
    the pop-up (and put a message in the Messages tab of the
    Error Console).
    
    Note: the htmlpopup or dev branch will need a similar fix done
    for this._originalCursorNode.

--- src/chrome/content/overlay.js
+++ src/chrome/content/overlay.js
@@ -523,6 +523,13 @@ ThumbnailZoomPlusChrome.Overlay = {
       // Detected that the user loaded a different page into our window, e.g.
       // by clicking a link.  So close the popup.
       this._logger.debug("_handlePageLoaded: *** closing since a page loaded into its host window");
+
+      // A new document has been loaded so we can't access nodes from the
+      // previous document anymore.  Clear them to avoid accessing dead
+      // data (issue #75).
+      this._panelCaption.ThumbnailZoomPlusOriginalTitleNode = null;
+      this._currentThumb = null;
+      
       this._closePanel(true);
     }
   },
@@ -1514,7 +1521,11 @@ ThumbnailZoomPlusChrome.Overlay = {
         this._imageObjectBeingLoaded.onerror = null;
         this._imageObjectBeingLoaded = null;
       }
-
+    } catch (e) {
+      this._logger.debug("_closePanel @1: caught EXCEPTION: " + e);
+      this._logToConsole("ThumbnailZoomPlus: _closePanel @1: caught EXCEPTION: " + e);
+    }
+    try {
       this._originalURI = "";
       this._hideThePopup();
 
@@ -1525,8 +1536,8 @@ ThumbnailZoomPlusChrome.Overlay = {
       this._panelImage.removeAttribute("src");
       this._currentThumb = null;
     } catch (e) {
-      this._logger.debug("_closePanel: caught EXCEPTION: " + e);
-      this._logToConsole("ThumbnailZoomPlus: _closePanel: caught EXCEPTION: " + e);
+      this._logger.debug("_closePanel @2: caught EXCEPTION: " + e);
+      this._logToConsole("ThumbnailZoomPlus: _closePanel @2: caught EXCEPTION: " + e);
     }
   },
 
