dadler/thumbnail-zoom

commit 4afed51c40db62a50ad99b366528bc1aebd64770

Fixed bug introduced in htmlpopup: was restarting gif movie a second after it started Eliminates temporary new Image object.

src/chrome/content/overlay.js
@@ -2093,8 +2093,13 @@ ThumbnailZoomPlusChrome.Overlay = {
     
     let thumbWidth = aImageNode.clientWidth;
     let thumbHeight = aImageNode.clientHeight;
-    let imageWidth = image.width;
-    let imageHeight = image.height;
+    
+    /*
+     * Get image size from naturalWidth, which tells us the image's true
+     * size, uninfluenced by CSS
+     */
+    let imageWidth = image.naturalWidth;
+    let imageHeight = image.naturalHeight;
     if (imageWidth == 0 || imageHeight == 0) {
       // Some images (such as .svg Scalable Vector Graphics) don't always have
       // an explicit size.  Give it an arbitrary resolution, at which it'll
@@ -2142,10 +2147,6 @@ ThumbnailZoomPlusChrome.Overlay = {
         this._hideCaption();
       }
     }
-    // Help the garbage collector reclaim memory quickly.
-    // (Test by watching "images" size in about:memory.)
-    image.src = null;
-    image = null;
   },
 
   _redisplayPopup : function()
@@ -2310,7 +2311,7 @@ ThumbnailZoomPlusChrome.Overlay = {
        partially loaded.  Using a separate Image node allows us to show
        the partial image sooner. 
      */
-    let image = new Image();
+    let image = this._panelImage;
     that._imageObjectBeingLoaded = image;
     
     let pageZoom = gBrowser.selectedBrowser.markupDocumentViewer.fullZoom;
@@ -2342,7 +2343,6 @@ ThumbnailZoomPlusChrome.Overlay = {
       that._imageObjectBeingLoaded = null;
     };
 
-    this._panelImage.src = aImageSrc;
     image.src = aImageSrc;
 
     /*

commit 4e2287e503ab5526896a8979c8327c2b12d54017

Fixed problem introduced in 4afed51c which prevented image from displaying before completely loaded.  But causes gif images to restart from the beginning when they're completely loaded; see Firefox bug 756367.

src/chrome/content/overlay.js
@@ -2048,7 +2048,7 @@ ThumbnailZoomPlusChrome.Overlay = {
     this._logger.debug("_checkIfImageLoaded: showing popup as 'working' indicator.");
     this._showStatusIcon(aImageNode, "working.png", 16);      
     
-    if (image.width > 0 && image.height > 0) {
+    if (image.naturalWidth > 0 && image.naturalHeight > 0) {
       /*
        * The image has a size so we could technically display it now.  But that
        * often causes it to appear very briefly only half-displayed, with