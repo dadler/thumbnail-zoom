885555bf1b1d3d5501a844051516c7c29ad418a7
David Adler <browser.add.ons@davidadler.name>
2012-08-11 00:48:00 -0700

    Fixed google.hu and other google search sites without ".com" to not show popup when hovering over logo or google doodle.

--- src/resources/pages.js
+++ src/resources/pages.js
@@ -1306,7 +1306,7 @@ ThumbnailZoomPlus.Pages.Thumbnail = {
   // Patterns in () must match starting from first slash (or earlier)
   // up to end of entire URL, so typically start with // and end with .* .
   imageRegExp: new RegExp("^(?![^/]*("
-                          + "(//.*\\.google\\.com?[.a-z]*/(.*/)?(images|logos)/)" // google logos
+                          + "(//.*\\.google\\.(com?(\\.[a-z]+)?|[a-z]+)/(.*/)?(images|logos)/)" // google logos
                           + "|(//[a-z0-9]+\\.google\\.com?[.a-z]*/.*[/?&]lyrs=.*)" // google maps tiles
                           + "|(//maps\\.google\\.com?[.a-z]*/.*)" // google maps user photo popups, etc.
                           + "|(//.*\\.gstatic\\.com?[.a-z]*/.*)" // google maps button images, google drive file type icons
