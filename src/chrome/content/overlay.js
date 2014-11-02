4f7250605f96b315849121cafaac276e70852c78
David Adler <browser.add.ons@davidadler.name>
2014-11-01 22:29:21 -0700

    thinner pop-up border so it doesn't contrast as much with page background & photo

--- src/chrome/content/overlay.js
+++ src/chrome/content/overlay.js
@@ -189,7 +189,7 @@ ThumbnailZoomPlusChrome.Overlay = {
   // and the popup when the border is displayed.  This must equal the
   // #thumbnailzoomplus-panel "padding" value in overlay.css.
   // Debugging tip: set large (30) when debugging popup positioning.
-  _borderWidth : 5, // border itself adds 5 pixels on each edge.
+  _borderWidth : 2, // border itself adds this many pixels on each edge.
   
   // _widthAddon is additional image width due to border if enabled:
   // 0 or _borderWidth*2.
