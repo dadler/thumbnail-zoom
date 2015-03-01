dadler/thumbnail-zoom

commit bb4ea72c827509cd4c8a35777c01fa2444d7fff4

Fixed bug where context menu sometimes didn't appear, e.g. when using Shift as described in a comment on #144. The fix ensures that _postponeContextMenuEvent is always set true after _handleContextMenu().

src/chrome/content/overlay.js
@@ -260,6 +260,10 @@ ThumbnailZoomPlusChrome.Overlay = {
    * If set to true, the _handleContextMenu will cause the next contextmenu event
    * originated form RMB mouse event to be postponed. When false, the
    * context menu is allowed to be handled normally.
+   *
+   * This is normally true, but temporarily set to false when we send the
+   * synthetic event which we want to actually cause a context menu to appear;
+   * that false prevents our own handler from intercepting our synthetic event.
    */
   _postponeContextMenuEvent : true,
 
@@ -4345,9 +4349,25 @@ ThumbnailZoomPlusChrome.Overlay = {
 
   /**
    * Listening to all contextmenu events per document.
+   *
+   * Context menu event may be fired after:
+   * - mousedown event (Linux)
+   * - mouseup event (Windows)
+   * For RMB feature it is essential to postpone the context menu after the
+   * mouseup event.
    */
   _handleContextMenu : function(aDocument, aEvent, aPage) {
     this._logger.trace("_handleContextMenu on " + aEvent.target);
+    var origPostponeContextMenuEvent = this._postponeContextMenuEvent;
+    this._postponeContextMenuEvent = true;
+    if (! origPostponeContextMenuEvent) {
+        /*
+         * We are skipping this contextmenu event and it will continue to propagate.
+         * However the contextmenu could still be prevented from
+         * appearing by us (see _preventNextContextMenuEvent) or something else.
+         */
+         return;
+    }
 
     if (
       (aEvent.buttons & 2) > 0 ||
@@ -4379,15 +4399,6 @@ ThumbnailZoomPlusChrome.Overlay = {
         aEvent.stopPropagation();
         aEvent.preventDefault();
         this._postponedContextMenuEvent = aEvent;
-      } else {
-        /*
-         * We are skipping this contextmenu event and it will continue to propagate.
-         * However the contextmenu could still be prevented from
-         * appearing by us (see _preventNextContextMenuEvent) or something else.
-         */
-        this._logger.debug("_handleContextMenu: enabling _postponeContextMenuEvent.");
-        this._postponeContextMenuEvent = true;
-      }
     }
   },
 
@@ -4410,9 +4421,6 @@ ThumbnailZoomPlusChrome.Overlay = {
        * RMB was released. We can let all contextmenu events after this point to
        * propagate. If we have "paused" a contextmenu event before, resume it now.
        */
-      this._logger.debug("_handleMouseUp: disabling _postponeContextMenuEvent.");
-      this._postponeContextMenuEvent = false;
-
       if (this._postponedContextMenuEvent != null) {
         // the default action of the event has been prevented, lets make clone
         // of the event and dispatch it again
@@ -4426,6 +4434,9 @@ ThumbnailZoomPlusChrome.Overlay = {
    */
   _redispatchMouseEvent : function(aMouseEvent) {
     this._logger.trace("_redispatchMouseEvent");
+
+    this._logger.debug("_handleMouseUp: disabling _postponeContextMenuEvent.");
+    this._postponeContextMenuEvent = false;
 
     // a side-effect of redispatching the event seems to be preventing
     // spell-check suggestions.  The problem happens even if we create the

commit 7872725efcae7960b946d86bb5d9672367002fb5

Rollback of bb4ea72c827509cd4c8a35777c01fa2444d7fff4
The bb4ea72c827509cd4c8a35777c01fa2444d7fff4 broke RMB handling in Windows
(and quite possibly in Linux too).

src/chrome/content/overlay.js
@@ -261,10 +261,6 @@ ThumbnailZoomPlusChrome.Overlay = {
    * If set to true, the _handleContextMenu will cause the next contextmenu event
    * originated form RMB mouse event to be postponed. When false, the
    * context menu is allowed to be handled normally.
-   *
-   * This is normally true, but temporarily set to false when we send the
-   * synthetic event which we want to actually cause a context menu to appear;
-   * that false prevents our own handler from intercepting our synthetic event.
    */
   _postponeContextMenuEvent : true,
 
@@ -4402,16 +4398,6 @@ ThumbnailZoomPlusChrome.Overlay = {
    */
   _handleContextMenu : function(aDocument, aEvent, aPage) {
     this._logger.trace("_handleContextMenu on " + aEvent.target);
-    var origPostponeContextMenuEvent = this._postponeContextMenuEvent;
-    this._postponeContextMenuEvent = true;
-    if (! origPostponeContextMenuEvent) {
-        /*
-         * We are skipping this contextmenu event and it will continue to propagate.
-         * However the contextmenu could still be prevented from
-         * appearing by us (see _preventNextContextMenuEvent) or something else.
-         */
-         return;
-    }
 
     if (
       (aEvent.buttons & 2) > 0 ||
@@ -4443,6 +4429,19 @@ ThumbnailZoomPlusChrome.Overlay = {
         aEvent.stopPropagation();
         aEvent.preventDefault();
         this._postponedContextMenuEvent = aEvent;
+      } else {
+        /*
+         * We are skipping this contextmenu event and it will continue to propagate.
+         * Because we want to postpone all context menu events (triggered by mouse)
+         * including the next one we need to reset _postponeContextMenuEvent to default
+         * value (true);
+         * Note: just because we are skipping postpone operation on context menu
+         * event it doesn't mean we want to skip prevent operation on context menu event
+         * (see _preventNextContextMenuEvent).
+         */
+        this._logger.debug("_handleContextMenu: enabling _postponeContextMenuEvent.");
+        this._postponeContextMenuEvent = true;
+      }
     }
   },
 
@@ -4465,6 +4464,9 @@ ThumbnailZoomPlusChrome.Overlay = {
        * RMB was released. We can let all contextmenu events after this point to
        * propagate. If we have "paused" a contextmenu event before, resume it now.
        */
+      this._logger.debug("_handleMouseUp: disabling _postponeContextMenuEvent.");
+      this._postponeContextMenuEvent = false;
+
       if (this._postponedContextMenuEvent != null) {
         // the default action of the event has been prevented, lets make clone
         // of the event and dispatch it again
@@ -4478,9 +4480,6 @@ ThumbnailZoomPlusChrome.Overlay = {
    */
   _redispatchMouseEvent : function(aMouseEvent) {
     this._logger.trace("_redispatchMouseEvent");
-
-    this._logger.debug("_handleMouseUp: disabling _postponeContextMenuEvent.");
-    this._postponeContextMenuEvent = false;
 
     var freshMouseEvent = document.createEvent("MouseEvents");
     freshMouseEvent.initMouseEvent(